5e3d4433096ebc4a0fefa97350495295e08010a1
Hritik Mahirrao <hritiknarendra@gmail.com>
2021-11-27 16:09:48 +0530

    Basic setup for weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const api = {
+  key : "6e9c6667a6257f375fb4cca660dcb2e1",
+  base: "api.openweathermap.org/data/2.5/",
+};
+
+var today = new Date();
+var date  = 
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="card">
+          <div className="wrapper">
+            <div className = "search-box">
+              <input
+                type        = "text"
+                className   = "search-bar"
+                placeholder = "Search..."
+              />
+            </div>
+            <div className="information">
+              <div className = "name">Pune</div>
+              <div className = "temp">29°c</div>
+              <div className = "date">{date}</div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
